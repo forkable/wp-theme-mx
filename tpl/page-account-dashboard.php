fd7066a9b2de5a7c4196d41f32a409996e36c5fa
Km.Van <kmvan.com@gmail.com>
2015-08-13 00:36:27 +0800

    zoom module padding

--- tpl/page-account-dashboard.php
+++ tpl/page-account-dashboard.php
@@ -1,17 +1,13 @@
-<div class="panel panel-default">
-	<div class="panel-body">
-		<div class="row">
-			<?php 
-			$dashboards = array(
-				'left' => 'col-md-4 col-lg-4',
-				'right' => 'col-md-8 col-lg-8',
-				
-			);
-			foreach($dashboards as $k => $v){ ?>
-				<div class="account-dashboard account-dashboard-<?= $k;?> <?= $v;?>">
-					<?php do_action("account_dashboard_{$k}");?>
-				</div>
-			<?php } ?>
+<div class="row">
+	<?php 
+	$dashboards = array(
+		'left' => 'col-md-4 col-lg-4',
+		'right' => 'col-md-8 col-lg-8',
+		
+	);
+	foreach($dashboards as $k => $v){ ?>
+		<div class="account-dashboard account-dashboard-<?= $k;?> <?= $v;?>">
+			<?php do_action("account_dashboard_{$k}");?>
 		</div>
-	</div>
+	<?php } ?>
 </div>
